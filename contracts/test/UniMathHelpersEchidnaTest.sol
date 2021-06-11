ab03464495e16686f13fe5cd21d0a39e13505dd5
Denis Glotov <denis.g@p2p.org>
2021-06-11 10:50:34 +0000

    more explicit code, first path works

--- contracts/test/UniMathHelpersEchidnaTest.sol
+++ contracts/test/UniMathHelpersEchidnaTest.sol
@@ -8,19 +8,25 @@ contract UniMathHelpersEchidnaTest {
     address constant FIRST = 0x0000000000000000000000000000000000000001;
     address constant SECOND = 0x0000000000000000000000000000000000000002;
     uint256 constant PRECISION = 1 << 192; // square of Q64.96 type
-    
+    uint256 constant MASK = ~uint256(0xFF);
+
     function checkGetQuoteFromSqrt(
         uint160 sqrtRatioX96,
-        uint128 baseAmount,
-        bool tokensOrder
+        uint128 baseAmount
     ) public pure {
-        uint256 res = UniMathHelpers.getQuoteFromSqrt(sqrtRatioX96, baseAmount, tokensOrder ? FIRST : SECOND, tokensOrder ? SECOND : FIRST);
+        uint256 res = UniMathHelpers.getQuoteFromSqrt(sqrtRatioX96, baseAmount, FIRST, SECOND);
 
-        uint256 ratioX192 = uint256(sqrtRatioX96) * sqrtRatioX96;
-        if (tokensOrder) {
-            assert(res == ratioX192 * baseAmount / PRECISION);
-        } else {
-            assert(res == uint256(baseAmount) * PRECISION / ratioX192);
-        }
+        uint256 ratioX128 = FullMath.mulDiv(sqrtRatioX96, sqrtRatioX96, 1 << 64);
+        assert(res & MASK == FullMath.mulDiv(ratioX128, baseAmount, 1 << 128) & MASK);
+    }
+
+    function checkReverseGetQuoteFromSqrt(
+        uint160 sqrtRatioX96,
+        uint128 baseAmount
+    ) public pure {
+        uint256 res = UniMathHelpers.getQuoteFromSqrt(sqrtRatioX96, baseAmount, SECOND, FIRST);
+
+        uint256 ratioX128 = FullMath.mulDiv(sqrtRatioX96, sqrtRatioX96, 1 << 64);
+        assert(res & MASK == FullMath.mulDiv(baseAmount, 1 << 128, ratioX128) & MASK);
     }
 }
